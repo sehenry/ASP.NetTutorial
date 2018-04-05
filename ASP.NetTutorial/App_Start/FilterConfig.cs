594c31564ff67490fc79d67007bd9c0e9b3bb520
sehenry <sarahelhe@gmail.com>
2018-04-04 20:20:28 -0500

    ASP.Net framework set up, using MVC.

---
+++ ASP.NetTutorial/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NetTutorial
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ASP.NetTutorial/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP.NetTutorial.MvcApplication" Language="C#" %>
